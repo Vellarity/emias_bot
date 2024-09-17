39032093dff6774b8062617fe10015cc18b6173f
Balduev Nikita <balduev@ntcms.ru>
2024-09-17 12:29:18 +0300

    Added same buttons functionality
    Signed-off-by: Balduev Nikita <balduev@ntcms.ru>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use dotenv::dotenv;
+use parsable::doctors;
 use std::{env, error::Error, sync::Arc};
 use teloxide::{dispatching::dialogue::GetChatId, prelude::*, types::{InlineKeyboardButton, InlineKeyboardMarkup, Me}, utils::command::BotCommands};
 use sea_orm::{ColumnTrait, ConnectOptions, Database, DatabaseConnection, EntityTrait, QueryFilter};
@@ -9,7 +10,7 @@ use entities::{info, prelude::*};
 pub mod parsable;
 
 pub mod helper;
-use helper::{get_referrals_obj, get_user_referrals};
+use helper::{get_doctors_obj, get_referrals_obj, get_user_referrals};
 
 pub mod em_commands;
 
@@ -135,9 +136,65 @@ async fn callback_handler(bot: Bot, callback: CallbackQuery ) -> Result<(), Box<
                 }
             },
             "back_to_main" => {
-
+                let go_to_ref_button = InlineKeyboardButton::new(
+                    "Записаться", 
+                    teloxide::types::InlineKeyboardButtonKind::CallbackData("get_referrals".to_string())
+                );
+                let markup = InlineKeyboardMarkup::new([[go_to_ref_button]]);
+                bot.edit_message_reply_markup(chat_id, callback.message.unwrap().id()).reply_markup(markup).await.unwrap();
             },
-            _ => {}
+            "get_doctors" => {
+                let referral_id:u64 = command_parts[1].parse().unwrap();
+                let docs_result = get_doctors_obj(&user, &referral_id).await;
+
+                match docs_result {
+                    Ok(doctors) => {
+                        let away_key=InlineKeyboardButton::new("Назад", teloxide::types::InlineKeyboardButtonKind::CallbackData("get_referrals".to_string()));
+
+                        match doctors.result {
+                            doctors::ResultType::DocArray(doctors) => {
+                                let mut doc_vec = vec![];
+                                for doctor in doctors {
+                                    let doc_button = InlineKeyboardButton::new(
+                                        &format!("- {} {} {}", doctor.main_doctor.first_name, doctor.main_doctor.second_name, doctor.main_doctor.last_name), 
+                                        teloxide::types::InlineKeyboardButtonKind::CallbackData("get_shedule".to_string()));
+                                    
+                                    doc_vec.push([doc_button]);
+                                }
+
+                                doc_vec.push([away_key]);
+                                let markup = InlineKeyboardMarkup::new(doc_vec);
+                                bot.edit_message_reply_markup(chat_id, callback.message.unwrap().id()).reply_markup(markup).await.unwrap();
+                            },
+                            doctors::ResultType::LdpArray(ldps) => {
+                                let mut ldp_vec = vec![];
+                                for ldp in ldps {
+                                    let ldp_button = InlineKeyboardButton::new(
+                                        &format!("- {}: \n", ldp.name), 
+                                        teloxide::types::InlineKeyboardButtonKind::CallbackData("get_shedule".to_string()));
+                                    
+                                    ldp_vec.push([ldp_button]);
+                                }
+
+                                ldp_vec.push([away_key]);
+                                let markup = InlineKeyboardMarkup::new(ldp_vec);
+                                bot.edit_message_reply_markup(chat_id, callback.message.unwrap().id()).reply_markup(markup).await.unwrap();
+                            },
+                            doctors::ResultType::EmptyObject(_) => {
+                                let no_doc = InlineKeyboardButton::new("Нет врачей по данному направлению.", teloxide::types::InlineKeyboardButtonKind::CallbackData("_".to_string()));
+                                let markup = InlineKeyboardMarkup::new([[no_doc], [away_key]]);
+                                bot.edit_message_reply_markup(chat_id, callback.message.unwrap().id()).reply_markup(markup).await.unwrap();
+                            }
+                        }
+                    },
+                    Err(_) => {
+                        bot.send_message(chat_id, "Не удалось получить список врачей").await.unwrap();
+                    }
+                }
+            }
+            _ => {
+
+            }
         }
     }
     
